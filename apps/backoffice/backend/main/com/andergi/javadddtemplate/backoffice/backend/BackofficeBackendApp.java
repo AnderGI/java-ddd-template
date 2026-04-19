d828657dc966835da5a6993593640ee2b1f9aaa1
andergi <agibarguen@gmail.com>
2026-04-19 19:39:05 +0200

    feat(:apps-backoffice-backend) register user happy path test

--- apps/backoffice/backend/main/com/andergi/javadddtemplate/backoffice/backend/BackofficeBackendApp.java
+++ apps/backoffice/backend/main/com/andergi/javadddtemplate/backoffice/backend/BackofficeBackendApp.java
@@ -3,7 +3,11 @@ package com.andergi.javadddtemplate.backoffice.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.andergi.javadddtemplate.src.backoffice", "com.andergi.javadddtemplate.src.shared"})
+@SpringBootApplication(scanBasePackages = {
+        "com.andergi.javadddtemplate.src.backoffice",
+        "com.andergi.javadddtemplate.src.shared",
+        "com.andergi.javadddtemplate.backoffice.backend"
+})
 public class BackofficeBackendApp {
     public static void main(String[] args) {
         SpringApplication.run(BackofficeBackendApp.class, args);
